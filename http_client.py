88f5a77482f810870ae24ffe11d68f0f1175c470
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-03-13 14:23:27 -0700

    Add response.raw

--- http_client.py
+++ http_client.py
@@ -2,25 +2,45 @@ import usocket
 import ujson
 try:
     import ussl
+    SUPPORT_SSL = True
 except ImportError:
     ussl = None
+    SUPPORT_SSL = False
 
-SUPPORT_SSL = ussl is not None
 SUPPORT_TIMEOUT = hasattr(usocket.socket, 'settimeout')
-CONTENT_TYPE_JSON = "application/json"
+CONTENT_TYPE_JSON = 'application/json'
 
 
 class Response(object):
-    def __init__(self, status_code, content):
+    def __init__(self, status_code, raw):
         self.status_code = status_code
-        self.content = content
+        self.raw = raw
+        self._content = False
+        self.encoding = 'utf-8'
 
     @property
-    def text(self, encoding='utf-8'):
-        return self.content.decode(encoding)
+    def content(self):
+        if self._content is False:
+            self._content = self.raw.read()
+            self.raw.close()
+            self.raw = None
+
+        return self._content
+
+    @property
+    def text(self):
+        content = self.content
+
+        return str(content, self.encoding) if content else ''
+
+    def close(self):
+        if self.raw is not None:
+            self._content = None
+            self.raw.close()
+            self.raw = None
 
     def json(self):
-        return ujson.loads(self.content)
+        return ujson.loads(self.text)
 
     def raise_for_status(self):
         if 400 <= self.status_code < 500:
@@ -30,8 +50,7 @@ class Response(object):
 
 
 # Adapted from upip
-def request(method, url, json=None, timeout=None, headers=None,
-            skip_body=False):
+def request(method, url, json=None, timeout=None, headers=None):
     urlparts = url.split('/', 3)
     proto = urlparts[0]
     host = urlparts[2]
@@ -63,47 +82,34 @@ def request(method, url, json=None, timeout=None, headers=None,
         assert SUPPORT_TIMEOUT, 'Socket does not support timeout'
         sock.settimeout(timeout)
 
-    try:
-        sock.connect(addr)
+    sock.connect(addr)
 
-        if proto == 'https:':
-            assert SUPPORT_SSL, 'HTTPS not supported: could not find ussl'
-            sock = ussl.wrap_socket(sock)
+    if proto == 'https:':
+        assert SUPPORT_SSL, 'HTTPS not supported: could not find ussl'
+        sock = ussl.wrap_socket(sock)
 
-        # MicroPython rawsocket module supports file interface directly
-        sock.write('%s /%s HTTP/1.0\r\nHost: %s\r\n' % (method, urlpath, host))
+    sock.write('%s /%s HTTP/1.0\r\nHost: %s\r\n' % (method, urlpath, host))
 
-        if headers is not None:
-            for header in headers.items():
-                sock.write('%s: %s\r\n' % header)
+    if headers is not None:
+        for header in headers.items():
+            sock.write('%s: %s\r\n' % header)
 
-        if content is not None:
-            sock.write('content-length: %s\r\n' % len(content))
-            sock.write('content-type: %s\r\n' % content_type)
-            sock.write('\r\n')
-            sock.write(content)
-        else:
-            sock.write('\r\n')
+    if content is not None:
+        sock.write('content-length: %s\r\n' % len(content))
+        sock.write('content-type: %s\r\n' % content_type)
+        sock.write('\r\n')
+        sock.write(content)
+    else:
+        sock.write('\r\n')
 
-        l = sock.readline()
-        protover, status, msg = l.split(None, 2)
+    l = sock.readline()
+    protover, status, msg = l.split(None, 2)
 
-        # Skip headers
-        while sock.readline() != b'\r\n':
-            pass
+    # Skip headers
+    while sock.readline() != b'\r\n':
+        pass
 
-        content = b''
-
-        if not skip_body:
-            while 1:
-                l = sock.read(1024)
-                if not l:
-                    break
-                content += l
-
-        return Response(int(status), content)
-    finally:
-        sock.close()
+    return Response(int(status), sock)
 
 
 def get(url, **kwargs):
